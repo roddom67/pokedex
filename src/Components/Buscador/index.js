ec2338d65b2877e7e260f4484d34c3395cc8e064
roddom67 <roddom67@gmail.com>
2019-09-20 21:12:23 -0300

    ajustes

--- src/Components/Buscador/index.js
+++ src/Components/Buscador/index.js
@@ -2,31 +2,23 @@ import React, { Component} from 'react';
 import styles from './Buscador.module.css';
 
 export class Buscador extends Component{
-	constructor(props){
-		super(props);
-		this.state = {
-
-		}
-	}
-
-	handleClick = (e) => {
-		return ;
-	}
-
 	render(){
 		return(
-			<form>
-			 <fieldset>
-			 	<ul className={styles.listado}>
-			 		<li>
-			 			<label>Nombre o número de Pokémon</label>
-			 			<input  />
-			 		</li>
-			 		<li>
-			 			<button onClick={this.handleClick} >Buscar</button>
-			 		</li>
-			 	</ul>
-			 </fieldset>
+			<form onSubmit={this.props.handleSubmit}>
+			 	<fieldset>
+				 	<ul className={styles.listado}>
+				 		<li>
+				 			<label htmlFor="pokemon">Nombre o número de Pokémon</label>
+				 			<input 
+				 				id="pokemon"
+								value={this.props.value}
+								onChange={this.props.handleChange} />
+				 		</li>
+				 		<li>
+				 			<button>Buscar</button>
+				 		</li>
+				 	</ul>
+				</fieldset>
 			</form>
 		);
 	}
--- src/Components/Pokedex/index.js
+++ src/Components/Pokedex/index.js
@@ -7,11 +7,51 @@ export class Pokedex extends Component{
 	constructor(props){
 		super(props);
 		this.state = {
-			valor: ''
-		}
+			title: "Pokédex",
+			value: '',
+			todos: [],
+			submit: false,
+			findPokemon: ''
+		};
+		//this.handleChange = this.handleChange.bind(this);
 	}
 
+	handleChange = (e) => {
+		const value = e.target.value;
+		
+		this.setState({
+			value: value
+		});		
+	}
+	handleSubmit = (e) => {
+		e.preventDefault();
+		const ajaxFile = 'https://pokeapi.co/api/v2/pokemon/' + this.state.value;
+		console.log(ajaxFile)
+		console.log('https://pokeapi.co/api/v2/pokemon/' + this.state.value);
+		this.setState({
+			submit:true,
+			findPokemon: ajaxFile
+		})
+		console.log(this.state.submit)
+	}
 
+	//https://pokeapi.co/api/v2/pokemon/ditto/
+	componentWillMount() {
+		if(this.state.submit){
+			//https://pokeapi.co/api/v2/pokemon/Numero o nombre
+			console.log(this.state.findPokemon);
+			fetch(this.state.findPokemon)
+				.then(res => res.json())
+				.then((data) => {
+			  		this.setState({ todos: data })
+			  		console.log(this.state.todos)
+				})
+				.catch(console.log)
+		}
+	}
+	componentWillReceiveProps(newProps){
+		console.log('Recibo cambios')
+	}
 
 	render(){
 
@@ -20,7 +60,10 @@ export class Pokedex extends Component{
 				<div className="row pt-5 pb-5">
 					<div className="">
 				      	<h1>PokéDex</h1>
-				      	<Buscador valor={this.state.valor}  />
+				      	<Buscador 
+				      		value={this.value}
+							handleChange={this.handleChange}
+							handleSubmit={this.handleSubmit}  />
 				      	<Resultado />
 				      	<Info />
 		      		</div>
